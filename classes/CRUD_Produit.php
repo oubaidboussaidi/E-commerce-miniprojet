307be8ee4a4c24adf549543df5b274f22f934471
Oubaid Boussaidi <oubaydboussaidi@gmail.com>
2024-11-13 11:33:51 +0100

    Initial commit

---
+++ classes/CRUD_Produit.php
@@ -0,0 +1,9 @@
+<?php
+class CRUD_Produit {
+    private $pdo;
+    function  __construct() {}
+    function  add(){}
+    function  delete(){}
+    function  find(){}
+    function findAll(){}
+    function edit(){}}
